605e08e48faad09a1a9073bb7af914163bc514af
jungminseo7812 <jungminseo7812@gmail.com>
2026-03-03 16:01:37 +0900

    feat: 로컬 폰트 변경

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -15,6 +15,20 @@ const geistMono = localFont({
   src: './fonts/GeistMonoVF.woff',
   variable: '--font-geist-mono',
 });
+const pretendard = localFont({
+  src: [
+    {path: './fonts/Pretendard-Black.otf', weight: '400'},
+    {path: './fonts/Pretendard-Bold.otf', weight: '500'},
+    {path: './fonts/Pretendard-ExtraBold.otf', weight: '600'},
+    {path: './fonts/Pretendard-Light.otf', weight: '100'},
+    {path: './fonts/Pretendard-ExtraLight.otf', weight: '0'},
+    {path: './fonts/Pretendard-Regular.otf', weight: '300'},
+    {path: './fonts/Pretendard-Medium.otf', weight: '200'},
+    {path: './fonts/Pretendard-SemiBold.otf', weight: '450'},
+    {path: './fonts/Pretendard-Thin.otf', weight: '100'},
+  ],
+  variable: '--font-pretendard',
+});
 
 export const metadata: Metadata = {
   title: 'Welcome to Vision AI GAMES World!',
@@ -28,7 +42,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={`${geistSans.variable} ${geistMono.variable} h-screen w-screen`}>
+      <body className={`${pretendard.variable} font-sans h-screen w-screen`}>
         <ThemeProvider attribute="data-theme" defaultTheme="dark">
           <AuthProvider>
             <div className="w-full px-4 py-2 flex justify-between gap-2 bg-amber-200 dark:bg-slate-900">
