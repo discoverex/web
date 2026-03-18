a20ea58378bbfe1f8abd5efbccfc6ef7aa51dca9
jungminseo7812 <jungminseo7812@gmail.com>
2026-03-18 10:14:56 +0900

    scoreboard

--- apps/magic-eye/app/components/views/game-board-content-view.tsx
+++ apps/magic-eye/app/components/views/game-board-content-view.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { AiHint, QuizCandidate } from '@/app/types';
 import { MovingAnswerOptions } from '../moving-answer-options';
 
