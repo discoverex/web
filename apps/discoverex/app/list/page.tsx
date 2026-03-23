58215957c27c48a6352d6fd897f85298917d95a3
김광회 <postelian@gmail.com>
2026-03-24 03:09:45 +0900

    fix: 빌드 오류

--- apps/discoverex/app/list/page.tsx
+++ apps/discoverex/app/list/page.tsx
@@ -1,6 +1,8 @@
 import ThemeListContainer from '@/app/components/theme-list-container';
 import { getThemes } from '@/app/api/theme-apis';
 
+export const dynamic = 'force-dynamic';
+
 export default async function Home() {
   const themes = await getThemes();
   return <ThemeListContainer themes={themes} />;
